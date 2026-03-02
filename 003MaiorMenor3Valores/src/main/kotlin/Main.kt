9252f422cd82fe2aabf1c3e7f7b1905ee383a8f3
osamuelelvis <elvislimasamuel27@gmail.com>
2026-03-02 14:55:04 -0300

    Exercício 3

---
+++ 003MaiorMenor3Valores/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    print("Informe o primeiro número: ")
+    val num1 = readln().toInt()
+    print("Ótimo! Agora informe o segundo número: ")
+    val num2 = readln().toInt()
+    print("Por fim, agora informe o terceiro número: ")
+    val num3 = readln().toInt()
+
+    if (num1 > num2 && num1 > num3) {
+        println("O número $num1 é maior.")
+    } else if (num2 > num3) {
+        println("O número $num2 é maior.")
+    } else {
+        println("O número $num3 é maior.")
+    }
+}
