f3808051aa6c5560b023b10a8028cbc98a33e820
osamuelelvis <elvislimasamuel27@gmail.com>
2026-03-03 09:52:55 -0300

    Exercício 7

---
+++ 007SomaMenorQue72/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var soma = 0
+
+    for (i in 1..6) {
+        print("Informe o $i º número: ")
+        val numero = readln().toInt()
+
+        println("Valor digitado: $numero")
+
+        if (numero < 72) {
+            soma += numero
+        }
+    }
+    print("A soma dos números menores que 72 é: $soma")
+}
