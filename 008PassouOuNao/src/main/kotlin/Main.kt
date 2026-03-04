c3f150c1455c69fca63e5aaa8f4d61125aed16c2
Samuel Elvis <169075141+osamuelelvis@users.noreply.github.com>
2026-03-04 11:26:56 -0300

    Update Main.kt

--- 008PassouOuNao/src/main/kotlin/Main.kt
+++ 008PassouOuNao/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+// 8. Escreva um programa que calcule a média de quatro números informados pelo usuário, mas somente se esses números forem maiores que 0 e menores que 10. No final, se a média for maior que cinco o usuário receberá uma mensagem "Você passou no teste". Em qualquer outra situação, ele receberá uma mensagem de "tente novamente"
 fun main() {
     var soma = 0;
 
